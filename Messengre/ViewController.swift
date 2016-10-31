a2bd37a136800d80fa960825fb9130e4a8e3efa8
Tim Myers <timmyers1978@gmail.com>
2016-10-30 23:32:58 -0600

    add setup view controller and have 3 screens now

--- Messengre/ViewController.swift
+++ Messengre/ViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var EmailField: UITextField!
+    @IBOutlet weak var PassField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +25,41 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    @IBAction func CreateAccount(_ sender: Any) {
+        
+        FIRAuth.auth()?.createUser(withEmail: EmailField.text!, password: PassField.text!, completion: {
+            user, error in
+            
+            if error != nil {
+                print("error")
+            } else {
+                print("User is Created")
+                self.Login()
+            }
+        })
+    }
+    
+    @IBAction func LoginPressed(_ sender: Any) {
+        
+    }
+    
+    func Login() {
+        FIRAuth.auth()?.signIn(withEmail: EmailField.text!, password: PassField.text!, completion: {
+            user, error in
+            
+            if error != nil {
+                print("error")
+            } else {
+                let vc = self.storyboard?.instantiateViewController(withIdentifier: "Profiles")
+                self.present(vc!, animated: true, completion: nil)
+            }
+            
+        })
+    }
+    
 
 
 }
---
+++ SetupView.swift
@@ -0,0 +1,18 @@
+//
+//  SetupView.swift
+//  Messengre
+//
+//  Created by Timothy Myers on 10/30/16.
+//  Copyright © 2016 okayestprogrammer. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class setupView: UIViewController {
+    
+    
+    
+    
+    
+}
